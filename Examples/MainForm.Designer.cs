b467289dcda641804fa46390691546d08f8643e6
CanadianBeaver <ontariobeaver@gmail.com>
2017-03-25 23:57:37 -0400

    Project initialization

--- Examples/MainForm.Designer.cs
+++ Examples/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Examples
 {
-  partial class Form1
+  partial class MainForm
   {
     /// <summary>
     /// Required designer variable.
@@ -28,9 +28,18 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.SuspendLayout();
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Text = "Form1";
+      this.ClientSize = new System.Drawing.Size(624, 442);
+      this.Name = "MainForm";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "DataGridViewExtenders examples";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
--- Examples/MainForm.cs
+++ Examples/MainForm.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Examples
 {
-  public partial class Form1 : Form
+  public partial class MainForm : Form
   {
-    public Form1()
+    public MainForm()
     {
       InitializeComponent();
     }
