3f20a66eeb8153d9063b1c7ef9f93d609e91927f
CanadianBeaver <ontariobeaver@gmail.com>
2017-03-25 23:49:16 -0400

    Project initialization

---
+++ DataViewExtendersTests/DataGridViewExtenders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+  [TestClass]
+  public class DataGridViewExtenders
+  {
+    [TestMethod]
+    public void AddColumnsTest()
+    {
+      Assert.Fail();
+    }
+  }
+}
---
+++ Source/DataGridViewExtenders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace CBComponents
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public static class DataGridViewExtenders
+  {
+
+    public static void AddColumns(this DataGridView viewGrid, object DataSource)
+    {
+
+    }
+
+  }
+}
