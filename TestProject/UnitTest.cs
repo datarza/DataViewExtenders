1d3a1f7159cbeef37bc44aaaf19ba9c03dd211af
CanadianBeaver <ontariobeaver@gmail.com>
2017-03-28 19:30:30 -0400

    New items for testing

---
+++ TestProject/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+  [TestClass]
+  public partial class UnitTest
+  {
+    [TestMethod]
+    public void TestObjects()
+    {
+      using (var _to = new TestObjects())
+      {
+        Assert.AreEqual(_to.DataGrid.Parent, _to.Form);
+        Assert.AreEqual(_to.Form.Location, new System.Drawing.Point(0, 0));
+        Assert.AreEqual(_to.Form.Width, 640);
+        Assert.AreEqual(_to.Form.Height, 480);
+        Assert.AreEqual(_to.DataGrid.Location, new System.Drawing.Point(0, 0));
+        Assert.AreEqual(_to.DataGrid.Width, 320);
+        Assert.AreEqual(_to.DataGrid.Height, 240);
+      }
+    }
+  }
+}
